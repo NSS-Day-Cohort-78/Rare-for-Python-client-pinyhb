4cda481b61c5c7b15a90324d36e0fbdae5483117
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-10-20 15:22:15 -0500

    added Post provider with context API

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,17 +1,32 @@
-import { Route, Routes } from "react-router-dom"
+import { Outlet, Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
+import { PostProvider } from "../components/posts/PostProvider"
+import { Posts } from "../components/posts/Posts"
 
 export const ApplicationViews = ({ token, setToken }) => {
-  return <>
-    <Routes>
-      <Route path="/login" element={<Login setToken={setToken} />}  />
-      <Route path="/register" element={<Register setToken={setToken} />}  />
-      <Route element={<Authorized token={token} />}>
-        {/* Add Routes here */}
-        
-      </Route>
-    </Routes>
-  </>
+	return (
+		<>
+			<Routes>
+				<Route path="/login" element={<Login setToken={setToken} />} />
+				<Route
+					path="/register"
+					element={<Register setToken={setToken} />}
+				/>
+				<Route element={<Authorized token={token} />}>
+					{/* Add Routes here */}
+					<Route
+						path="/"
+						element={
+							<PostProvider>
+								<Outlet />
+							</PostProvider>
+						}>
+						<Route index element={<Posts />} />
+					</Route>
+				</Route>
+			</Routes>
+		</>
+	)
 }
