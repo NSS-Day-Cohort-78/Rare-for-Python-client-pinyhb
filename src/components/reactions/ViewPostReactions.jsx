8c0caa4a0f07439dd3f0a512ac455c9b90f92fbd
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-10-24 11:33:49 -0500

    made fetch call dynamic

--- src/components/reactions/ViewPostReactions.jsx
+++ src/components/reactions/ViewPostReactions.jsx
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect } from "react"
 import { ReactionsContext } from "./ReactionsProvider"
 import "./reactions.css"
+import { useParams } from "react-router-dom"
 
 export const ViewPostReactions = () => {
 	const { postReactions, getReactionsByPostId } = useContext(ReactionsContext)
 
+	const { id } = useParams()
 	useEffect(() => {
-		getReactionsByPostId(1)
+		getReactionsByPostId(id)
 	}, [])
 	return (
 		<div className="reaction-border is-flex custom-flex-gap is-justify-content-space-between">
