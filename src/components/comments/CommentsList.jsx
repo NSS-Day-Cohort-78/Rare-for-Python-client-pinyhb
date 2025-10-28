b684d220ee5fa87d66975492c3ce2e5814f7e50e
McKenney Leavell <maleavell@gmail.com>
2025-10-28 09:59:06 -0500

    search by tag working

--- src/components/comments/CommentsList.jsx
+++ src/components/comments/CommentsList.jsx
@@ -7,21 +7,19 @@ export const CommentsList = ({ comment }) => {
 	const navigate = useNavigate()
 	const { token } = useContext(UserContext)
 	const { id } = useParams()
-  const currentUser = parseInt(token)
 	const [commentObj, setCommentObj] = useState({})
 	const [modal, setModal] = useState(false)
   
   useEffect(() => {
 		setCommentObj(comment)
 	}, [])
+
 	return (
-		<div className=" p-5 comment container is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
+		<div className="p-5 comment container is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
 			<h2>{comment.category.label}</h2>
 			<p>{comment.content}</p>
 			<div className="is-flex is-grouped is-flex-direction-row is-align-items-center">
 				<p>{comment.author.username}</p>
-				{/* <div className="is-justify-content-right">{currentUser === commentObj.author?.author_id ? <DeleteCommentButton id={comment.id}/> : ""}
-				</div> */}
 				{parseInt(token) === comment.author?.author_id ? (
 				<div className="is-flex is-align-items-center is-justify-content-center">
 					<i
